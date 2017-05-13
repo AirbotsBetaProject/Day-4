e55006ee61c9dd86f1ced726588aa02852d8eb95
Ayush995 <tabhane.ayush90@gmail.com>
2017-05-13 10:07:32 +0530

    PEP8 format and commenting

--- D4-02Ayush/Source Code/dwldimgbytext.py
+++ D4-02Ayush/Source Code/dwldimgbytext.py
@@ -2,20 +2,23 @@ import urllib
 import cv2
 import numpy as np
 import os
-
-text = open('links2.txt','r')
+# specify name of text file here and open as read
+text = open('links2.txt', 'r')
+# 'list' of all the links
 data = text.readlines()
+# update to suitable number
 pic_num = 3456
-for i in range(0,len(data)):
-        try:
-            print data[i]
-            urllib.urlretrieve(data[i], "neg2/"+str(pic_num)+".jpg")
-            img = cv2.imread("neg2/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
-    	    # should be larger than samples / pos pic (so we can place our image on it)	
-            resized_image = cv2.resize(img, (100, 100))
-            cv2.imwrite("neg2/"+str(pic_num)+".jpg",resized_image)
-            pic_num += 1
-            
-        except Exception as e:
-            print(str(e))
-text.close()
+for i in range(0, len(data)):
+    try:
+        print data[i]  # print the current link to be accessed
+        # download the image
+        urllib.urlretrieve(data[i], "neg2/"+str(pic_num)+".jpg")
+        img = cv2.imread("neg2/"+str(pic_num)+".jpg",
+                         cv2.IMREAD_GRAYSCALE)  # read as grayscale
+        resized_image = cv2.resize(img, (100, 100))  # reszie
+        cv2.imwrite("neg2/"+str(pic_num)+".jpg", resized_image)  # overwrite
+        pic_num += 1  # update pic_num for new file
+
+    except Exception as e:  # if anything above fails, throw an exception
+        print(str(e))
+text.close()  # close the text file
